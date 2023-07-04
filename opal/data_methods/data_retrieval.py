e3bc6663b0923f7842968f5ed4cd7ccff51ab60d
Phillip Paine <pmxpp88@gmail.com>
2023-07-04 00:42:30 -0700

    retrieve data
    finised and tested the retrieve data from locl or url

--- opal/data_methods/data_retrieval.py
+++ opal/data_methods/data_retrieval.py
@@ -1,21 +1,28 @@
 import pandas as pd
-from typing import Tuple
+import structlog
+from typing import Sequence
 from pathlib import Path
-from ..constants import DATA_REPO_URL, DATA_LOCAL_STORE
+from opal.constants import DATA_REPO_URL, DATA_LOCAL_STORE
 
 
-def retrieve_completed_seasons(filename: str, seasons: Tuple[str, ...] = ()) -> pd.DataFrame:
+logger = structlog.get_logger()
+
+
+def retrieve_completed_seasons(filename: str, filetype: str, seasons: Sequence[str] = ()) -> pd.DataFrame:
     """Return dataframe of requested completed seasons and filename. Cannot get partial seasons"""
     df_out = pd.DataFrame()
     for s_ in seasons:
         local_path = Path(DATA_LOCAL_STORE)
-        local_path.joinpath(filename)
-        if not local_path.exists:
+        local_file = local_path.joinpath('.'.join(['_'.join([filename, s_]), filetype]))
+        if not local_file.exists():
+            logger.info("File does not exist locally, retrieving from URL")
             # if no local file then download from url first:
-            url_path = Path(DATA_REPO_URL)
-            url_path.joinpath(filename)
+            url_path = '.'.join(['/'.join([DATA_REPO_URL, s_, filename]), filetype])
+            df = pd.read_csv(url_path)
 
-        df = get_data()
+            df.to_csv(local_file)
+
+        df = get_data(local_file)
         if df_out.empty:
             df_out = df
         else:
@@ -29,10 +36,15 @@ def retrieve_current_season(filename: str, season: str) -> pd.DataFrame:
     return df_out
 
 
-def get_data() -> pd.DataFrame:
-    return pd.DataFrame()
+def get_data(filepath: [str, Path]) -> pd.DataFrame:
+    return pd.read_csv(filepath)
 
 
 if __name__ == '__main__':
-    pass
+    seasons_sequence = ["2022-23"]
+    data_name = "teams"
+    data_type = "csv"
+    df_test = retrieve_completed_seasons(data_name, data_type, seasons_sequence)
+    print(df_test.head(1))
+    print(df_test.columns)
 
