46f94cd530e327986627a6ec340875c21c1bbc16
Phillip Paine <pmxpp88@gmail.com>
2023-07-05 00:20:32 -0700

    fix retrieval
    fix and complete the retrieve data from url or local

--- opal/data_methods/data_retrieval.py
+++ opal/data_methods/data_retrieval.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import structlog
-from typing import Sequence
+from typing import Sequence, Optional
 from pathlib import Path
 from opal.constants import DATA_REPO_URL, DATA_LOCAL_STORE
 
@@ -8,14 +8,15 @@ from opal.constants import DATA_REPO_URL, DATA_LOCAL_STORE
 logger = structlog.get_logger()
 
 
-def retrieve_completed_seasons(filename: str, filetype: str, seasons: Sequence[str] = ()) -> pd.DataFrame:
+def retrieve_completed_dataframes(filename: str, seasons: Sequence[str], filetype: Optional[str] = "csv") ->\
+        pd.DataFrame:
     """Return dataframe of requested completed seasons and filename. Cannot get partial seasons"""
     df_out = pd.DataFrame()
     for s_ in seasons:
         local_path = Path(DATA_LOCAL_STORE)
         local_file = local_path.joinpath('.'.join(['_'.join([filename, s_]), filetype]))
         if not local_file.exists():
-            logger.info("File does not exist locally, retrieving from URL")
+            logger.info(f"File does not exist locally, retrieving {'_'.join([s_, filename])} from URL")
             # if no local file then download from url first:
             url_path = '.'.join(['/'.join([DATA_REPO_URL, s_, filename]), filetype])
             df = pd.read_csv(url_path)
@@ -30,21 +31,31 @@ def retrieve_completed_seasons(filename: str, filetype: str, seasons: Sequence[s
     return df_out
 
 
-def retrieve_current_season(filename: str, season: str) -> pd.DataFrame:
-    """This is different because the season has not completed yet"""
-    df_out = pd.DataFrame()
-    return df_out
+def retrieve_active_dataframe(filename: str, season: str, filetype: Optional[str] = "csv") -> pd.DataFrame:
+    """This is different because the season has not completed yet, this means that we want to overwrite the dataframe
+    in certain occasions"""
+    local_path = Path(DATA_LOCAL_STORE)
+    local_file = local_path.joinpath('.'.join(['_'.join([filename, season]), filetype]))
+    logger.info(f"Retrieving {'_'.join([filename, season])} from URL")
+    url_path = '.'.join(['/'.join([DATA_REPO_URL, season, filename]), filetype])
+    df = pd.read_csv(url_path)
+    df.to_csv(local_file)
+    return df
 
 
 def get_data(filepath: [str, Path]) -> pd.DataFrame:
-    return pd.read_csv(filepath)
+    return pd.read_csv(filepath, index_col=0)
 
 
 if __name__ == '__main__':
-    seasons_sequence = ["2022-23"]
+    """Code tested: 
+    retrieve completed seasons works fine
+    retrieve active season ..."""
+    seasons_sequence = ["2021-22", "2022-23"]
     data_name = "teams"
     data_type = "csv"
-    df_test = retrieve_completed_seasons(data_name, data_type, seasons_sequence)
-    print(df_test.head(1))
+    df_test = retrieve_completed_dataframes(data_name, seasons_sequence, data_type)
+    print(df_test.sample(4))
     print(df_test.columns)
+
 
