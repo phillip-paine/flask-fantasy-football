a73d499b5501284c796c48ac0983e041a03a1287
phillip-paine <54201799+phillip-paine@users.noreply.github.com>
2023-07-18 22:26:20 -0700

    Create the tables and data for team strength (#3)
    team str data
    
    add the base classes for the team strength model data

--- opal/constants.py
+++ opal/constants.py
@@ -1,3 +1,4 @@
 DATA_REPO_URL = "https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/"
 DATA_LOCAL_STORE = "diamond/training_data/"
-
+PREVIOUS_SEASONS_LIST = ["2018-19", "2019-20", "2021-22", "2022-23"]
+CURRENT_SEASON_LIST = ["2023-24"]
